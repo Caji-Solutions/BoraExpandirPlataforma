463a6a9b6a35b3faf9d50e7d97980fb064b13aab
Nathan de Oliveira dos Santos <82466191+Nathan-dOS@users.noreply.github.com>
2026-03-11 11:38:04 -0300

    Adicionando arquivos desnecessarios no gitignore

--- backend/dist/controllers/health.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.healthController = void 0;
-exports.healthController = {
-    ping: (_req, res) => {
-        res.json({ success: true, message: 'pong', timestamp: new Date().toISOString() });
-    },
-};
--- backend/dist/middlewares/notFound.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = notFound;
-function notFound(_req, res, _next) {
-    res.status(404).json({ success: false, error: 'Rota não encontrada' });
-}
